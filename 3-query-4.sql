4620053dd2eab06cf23b5c510bdbc1f7750456d8
unknown <kevin.alvespinto@gmail.com>
2018-09-21 23:51:57 +0200

    3-query-4

--- 3-query-4.sql
+++ 3-query-4.sql
@@ -1,4 +1,10 @@
-select AVG(a.grade)
+select y.id as anneeEtude,ROUND(AVG(a.grade),2) as moyenne
 from assessment as a
+join enrolment as e
+on a.id_student = e.id_student
+join year as y
+on e.id_year = y.id
 join student as s
-on a.id_student=s.id;
+on s.id = a.id_student
+group by y.id
+order by moyenne DESC, lastname ASC; 
