0fbc3e1f5e1f379465cdbfa8e5a8339a1ce0a329
unknown <kevin.alvespinto@gmail.com>
2018-09-21 16:10:29 +0200

    3-query-2

---
+++ 3-query-2.sql
@@ -0,0 +1,8 @@
+select s.firstname,s.lastname,y.label
+from student as s
+join enrolment e
+on s.id = e.id_student
+left join year as y
+on e.id_year = y.id
+where e.`from` <= "2017-01-07 00:00:00"
+order by lastname ASC;
